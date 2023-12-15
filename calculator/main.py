8f837f2cbc156f2d40cb93987b0bb8aef7d66f2e
oscariotes <larrysuico2@gmail.com>
2023-12-15 14:09:02 +0800

    added art to calculator

--- calculator/main.py
+++ calculator/main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
 
@@ -10,25 +12,44 @@ def multiply(n1, n2):
 def devide(n1, n2):
     return n1 / n2
 
-num1 = int(input('What is the first number? '))
-num2 = int(input('What is the second number? '))
 
-operations = {
-'+': add, 
-'-' : subtract, 
-'*' : multiply, 
-'/': devide,
-}
+def calucalator():
+    print(logo)
 
-for symbols in operations:
-    print(symbols)
-    
 
-choose = str(input('Please choose the operation: '))
 
-answer = operations[choose](num1, num2)
-        
+    num1 = float(input('What is the first number? '))
 
-print(f'{num1} {choose} {num2} = {answer}')
+    operations = {
+    '+': add, 
+    '-' : subtract, 
+    '*' : multiply, 
+    '/': devide,
+    }
+
+    for symbols in operations:
+        print(symbols)
+
+    continue_calculation = True
+    while continue_calculation:
+        num2 = float(input('What is the next number? '))
+        choose = str(input('Please choose the operation: '))
+        answer = operations[choose](num1, num2)
+        print(f'{num1} {choose} {num2} = {answer}')
+
+        ask_user = str(input('Do you wannt to continue with another operation? y or n for new calculation ')).lower()
+        if ask_user == 'n':
+            continue_calculation = False
+            calucalator()
+
+        elif ask_user == 'y':
+            num1 = answer
+
+calucalator()
+
+
+
+
+
 
 
